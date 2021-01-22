1a57eff921e862edb0d1df6c757851e55b3b14d7
KissMárton <74532613+kissm@users.noreply.github.com>
2021-01-22 10:25:32 +0100

    Initial commit

---
+++ Methods.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class Methods {
+            static void tagolas(){
+                System.out.println("\n-------------------------------------------------------------\n");
+            }
+    public static void main(String[] args) {
+        tagolas();
+        System.out.println("A program neve ");
+        tagolas();
+    }
+    
+}
